6ccc1b6e6a1170cad7d598c381a5295fbed148f1
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2022-03-25 01:39:14 +0200

    New project

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import AppAside from './AppAside'
-import AppBreadcrumb from './AppBreadcrumb'
-import AppFooter from './AppFooter'
-import AppHeader from './AppHeader'
-import AppSidebar from './AppSidebar'
-
-export { AppAside, AppBreadcrumb, AppFooter, AppHeader, AppSidebar }
--- src/views/dashboard/Dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Dashboard = (): JSX.Element => {
-  return <>Hello World!</>
-}
-
-export default Dashboard
