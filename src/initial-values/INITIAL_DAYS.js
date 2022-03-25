d321adfbba1ca186088be6af129cc20249282b03
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2022-03-25 04:17:04 +0200

    Client project

---
+++ src/initial-values/INITIAL_DAYS.js
@@ -0,0 +1,37 @@
+export const INITIAL_DAYS = [
+  {
+    label: "Monday",
+    text: "Monday",
+    value: 1
+  },
+  {
+    label: "Tuesday",
+    text: "Tuesday",
+    value: 2
+  },
+  {
+    label: "Wednesday",
+    text: "Wednesday",
+    value: 3
+  },
+  {
+    label: "Thursday",
+    text: "Thursday",
+    value: 4
+  },
+  {
+    label: "Friday",
+    text: "Friday",
+    value: 5
+  },
+  {
+    label: "Saturday",
+    text: "Saturday",
+    value: 6
+  },
+  {
+    label: "Sunday",
+    text: "Sunday",
+    value: 0
+  }
+]
---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { withAuthenticationRequired, useAuth0 } from '@auth0/auth0-react'
+import { CCol, CFormSelect } from '@coreui/react'
+import { INITIAL_DAYS } from '../../initial-values/INITIAL_DAYS'
+import { auth0ApiCall } from 'src/api-ops/auth0';
+
+const date = new Date();
+let today = date.getDay();
+
+const Dashboard = () => {
+  const { user } = useAuth0();
+  const [userData, setUserData] = useState({ "user_metadata": { "role": 'N/A' } });
+  const [loading, setLoading] = useState("false");
+  const [selectedDay, setSelectedDay] = useState(today);
+  var selectedDayVar = today;
+
+  React.useEffect(() => {
+    setLoading(true);
+    Promise.resolve(
+      auth0ApiCall('GET', 'https://foodboot.eu.auth0.com/api/v2/users', 'q: ' + user.email + ', search_engine: v3', true)
+        .then(function (value) {
+          setUserData(value);
+          setLoading(false);
+        }));
+  }, []);
+
+  const handleChange = (e) => {
+    setSelectedDay(e.target.value);
+    selectedDayVar = e.target.value;
+    console.log(selectedDayVar)
+  }
+
+  return (
+    <>
+      <CCol sm={12}>
+        <CFormSelect value={selectedDay} options={INITIAL_DAYS}
+          onChange={handleChange} />
+      </CCol>
+    </>
+  )
+}
+
+export default withAuthenticationRequired(Dashboard)
